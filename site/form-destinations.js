4037d9b2892c4bc24c454688f7ea50b84a1a992c
ryancom16 <ryancom16@users.noreply.github.com>
2026-06-23 03:23:41 -0500

    Prefill registration with selected event

--- site/form-destinations.js
+++ site/form-destinations.js
@@ -2,7 +2,8 @@
 // the OncoPath Google account before using the site as the long-term intake.
 window.OncoPathFormDestinations = {
   registration:
-    "https://docs.google.com/forms/d/e/1FAIpQLSdO88Zl2yozquc9lVi8n6qBabVAj0-jJNTLWktserHhLqmN5Q/viewform?usp=sf_link",
+    "https://docs.google.com/forms/d/e/1FAIpQLSeViMKGfm8F9boIq_WkSEl60GPcaoRjHcfXkDczUWfF_xm-oQ/viewform?usp=sf_link",
+  registrationEventEntry: "entry.412987544",
   inquiry:
     "https://docs.google.com/forms/d/e/1FAIpQLSf4o3hDBS7BAPneLA3i0uYrqZXsqca3bCMmDwhgtFMOg9k9NQ/viewform?usp=sf_link",
 };
