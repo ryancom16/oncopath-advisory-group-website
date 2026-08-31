ee0ddf7aab17625bef3e49515ac1c873677d8d09
ryancom16 <ryancom16@users.noreply.github.com>
2026-08-31 18:59:18 -0500

    Connect public intake lanes to published forms

--- site/form-destinations.js
+++ site/form-destinations.js
@@ -1,9 +1,12 @@
-// Temporary tracer destinations. Replace these two URLs with forms owned by
-// the OncoPath Google account before using the site as the long-term intake.
+// Keep each public intake lane pointed at a form owned by the OncoPath Google
+// account. Sponsorship is intentionally separate from consulting so partner
+// submissions can be routed to partnerships@oncopathadvisorygroup.com.
 window.OncoPathFormDestinations = {
   registration:
-    "https://docs.google.com/forms/d/e/1FAIpQLSeViMKGfm8F9boIq_WkSEl60GPcaoRjHcfXkDczUWfF_xm-oQ/viewform?usp=sf_link",
-  registrationEventEntry: "entry.412987544",
+    "https://docs.google.com/forms/d/e/1FAIpQLSfMFBqnbmB04oSIcLgM4f_ZpBwjr7Tl2uK1qg6VJwmo_MPnlQ/viewform",
+  registrationEventEntry: "entry.1387437448",
   inquiry:
-    "https://docs.google.com/forms/d/e/1FAIpQLSf4o3hDBS7BAPneLA3i0uYrqZXsqca3bCMmDwhgtFMOg9k9NQ/viewform?usp=sf_link",
+    "https://docs.google.com/forms/d/e/1FAIpQLSeSCZ9zqHx54Xd86SCehSAZik7ZaCN8VxraRcGCa_5U_xhEiw/viewform",
+  sponsorship:
+    "https://docs.google.com/forms/d/e/1FAIpQLSex528Q8ktbHLhFKZlibk1mUArZB-CnVXM3I68FEemdRe4Ikg/viewform",
 };
